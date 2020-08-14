a87c454bacb1bd69a466ecbd6aaff730d966432b
sylene <sylene.manusset@gmail.com>
2020-08-14 17:34:01 +0200

    Add main.ts, entry point for the app

---
+++ app/main.ts
@@ -0,0 +1,4 @@
+// Create an entry point for the application
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app.module";
+platformBrowserDynamic().bootstrapModule(AppModule);
